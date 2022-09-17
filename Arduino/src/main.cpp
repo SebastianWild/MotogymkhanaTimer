138d2aa98a55a00ae1de47ecaa07fdbf3c5b934e
Sebastian Wild <sebastian@swild.dev>
2022-09-16 21:58:54 -0700

    Functional Ranging
    Add code to range things in centimeters

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,14 +1,23 @@
 #include <Arduino.h>
+#include <Ultrasonic.h>
+
+#define RANGERPIN 5
+const int DELAY = 100;    // msec to delay between readings
+Ultrasonic ultrasonic(RANGERPIN);
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  // open serial communication
+  Serial.begin(9600);
+  Serial.print("\r\nDistance: ");
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  // but actually the LED is on; this is because
-  // it is active low on the ESP-01)
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+  char _buffer[7];
+  int centimeters;
+
+  centimeters = ultrasonic.MeasureInCentimeters();
+  delay(DELAY);  // msec
+
+  sprintf(_buffer, "%03u cm", centimeters);
+  Serial.println(_buffer);  // print distance (in cm) on serial monitor
 }
